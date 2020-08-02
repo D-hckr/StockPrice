97e6204f66e4106f3997c6e1dda0428b757dc453
Prashams <podaulat@jcp.com>
2020-08-02 22:39:39 +0530

    resolved 'favicon' error

--- src/main/java/com/example/StockPrice/processClass.java
+++ src/main/java/com/example/StockPrice/processClass.java
@@ -12,19 +12,24 @@ public class processClass {
 
     public static List<String> stckList = new ArrayList<>();
     public List<String> getStockList(String requestString){
+     if(requestString.contains(",")) {
+         String[] tempList = requestString.split(",");
 
-        String[] tempList = requestString.split(",");
-
-        for(String str:tempList){
-            if(str.contains("IND")){
-                String tmp  = str.substring(0, str.length()-3);
-                stckList.add(tmp+"BO");
-                stckList.add(tmp+"NS");
-            }else{
-                String tmp1  = str.substring(0, str.length()-3);
-                stckList.add(tmp1);
-            }
-        }
+         for (String str : tempList) {
+             if (str.contains("IND")) {
+                 String tmp = str.substring(0, str.length() - 3);
+                 stckList.add(tmp + "BO");
+                 stckList.add(tmp + "NS");
+             } else {
+                 String tmp1 = str.substring(0, str.length() - 3);
+                 stckList.add(tmp1);
+             }
+         }
+     }else{
+         String tmp = requestString.substring(0, requestString.length() - 3);
+         stckList.add(tmp + "BO");
+         stckList.add(tmp + "NS");
+     }
         return stckList;
     }
 }
